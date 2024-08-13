d941d8dbdb32ef2cc28fff9b5edcfb74556e6b9c
yogamable <nicovelbaz@gmail.com>
2024-08-13 14:48:20 -0400

    prueba casi lista

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="d-block m-5">
+    <div>
+      <h1>Descubre tus Pokemon</h1>
+    </div>
+
+    <div class="row d-flex gap-3">
+      <PokemonCard v-for="(pokemon, index) in pokemones"
+      :key="index"
+      :pokemon="pokemon"
+      @descubrirPokemon="pokemonDescubierto"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import PokemonCard from './components/PokemonCard.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      pokemones: [],
+    }
+  },
+  
   components: {
-    HelloWorld
+    PokemonCard
+  },
+
+  async mounted (){
+    await this.obtenerPokemones();
+  },
+
+  methods: {
+    async obtenerPokemones(){
+      const url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
+      const responseApi = await axios.get(url);
+      const primerLlamado = responseApi.data.results;
+
+      //hay que llamar a cada uno de los 20 pokemones
+
+      const pokemones = await Promise.all(
+      primerLlamado.map(async (pokemon) => {
+      const { data } = await axios.get(pokemon.url);
+      return {
+        nombre: data.name,
+        imagen: data.sprites.front_default,
+        inputPokemon: data.inputPokemon,
+        
+      };
+      })
+    );
+      this.pokemones = pokemones;
+    },
+
+    pokemonDescubierto(){
+      
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,60 @@
+<template>
+  
+
+    <div class="card d-flex" style="width: 18rem;">
+      <div >
+        <img :src="pokemon.imagen" class="card-img-top" alt="pokemon.name" :style="{height: altura, backdropFilter: filtroImg}" >
+      </div>
+      <div class="card-body">
+        <h5 class="card-title text-center">¡Descubre su nombre!</h5>
+        <label class="d-flex justify-content-center" id="nombrePokemon" >{{ pokemon.nombre }}</label>
+        <div class="d-flex justify-content-center mt-2">
+          <input type="text" v-model="inputPokemon" />
+        </div>
+        <div class="d-flex justify-content-center">
+          <button type="submit" @click="descubrirPokemon()" class="btn btn-primary mt-2">Descubrir</button>
+        </div>        
+      </div>
+    </div>
+
+      
+</template>
+
+<script>
+
+export default {
+  name: 'PokemonCard',
+  data(){
+    return{
+      altura: '13rem',
+      inputPokemon: '',
+      filtroImg: "blur(10px)",
+    }
+  },
+
+  props: {
+    pokemon: Object,
+  },  
+
+  methods: {
+    
+    descubrirPokemon(){
+     if(this.pokemon.inputPokemon == this.pokemon.nombre){
+        alert("¡Felicitaciones, adivinaste!");
+        this.filtroImg = 'none';
+     }else {
+      alert("¡Te equivocaste!");
+     }
+    },
+
+  },
+}
+</script>
+
+<style scoped>
+
+img{
+  backdrop-filter: blur(10px);
+}
+
+</style>
